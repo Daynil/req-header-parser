dfa59aad6832c6ad1e044be91192495df3971f7c
Danny Libin <omegasol11@gmail.com>
2016-02-27 15:47:33 -0600

    fixed ip address to parse from ipv6 to ipv4

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const app = express();
 const morgan = require('morgan');
 const locale = require('locale');
 const uaParser = require('ua-parser-js');
+const ipaddr = require('ip-address');
 
 app.use(morgan('dev'));
 app.use(locale(['en', 'en_US']));
 
 app.get('/api/whoami/', (req, res) => {
-	let ip = req.ip;
+	let ip6 = new ipaddr.Address6(req.ip);
+	let ip = ip6.to4().address;
 	let language = req.locale;
 	let ua = uaParser(req.headers['user-agent']);
 	let software = `${ua.os.name} ${ua.os.version}; ${ua.cpu.architecture}`;
